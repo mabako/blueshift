48df8820ebbdb9652c9aa4f084fb5aa3cb33f343
mabako <mabako@gmail.com>
2012-06-19 21:33:27 +0200

    sandstorms in LS? we're not Fort Carson, man. Red sky in LS? This ain't Kubera.

--- resources/[ars]weather-system/s_weather.lua
+++ resources/[ars]weather-system/s_weather.lua
@@ -8,16 +8,7 @@ local function getData( theElement, key )
 	end
 end	
 
-local function setData( theElement, key, value, sync )
-	local key = tostring(key)
-	local value = tonumber(value) or tostring(value)
-	if isElement(theElement) and (key) and (value) then
-		
-		return exports['[ars]anticheat-system']:assignData( theElement, tostring(key), value, sync )
-	else
-		return false
-	end	
-end
+------------------------------------
 
 local weathers = 
 {
@@ -26,36 +17,21 @@ local weathers =
 	{ 2, "Sunny 3" },
 	{ 3, "Sunny 4" },
 	{ 7, "Sunny 5" },
-	{ 40, "Sunny 6" },
+	{ 11, "Sunny 6" },
 	{ 8, "Rainy" },
 	{ 10, "Clear" },
-	{ 19, "Sand Storm" }
+	{ 9, "Fog" },
 }
-
-local theWeatherID = nil
-local theWeatherName = nil
 
 -- CHANGE WEATHER
 local nextWeather = nil
 function regulateWeather( )
 	if ( nextWeather ~= nil ) then
-		
-		theWeatherID = weathers[nextWeather][1]
-		theWeatherName = weathers[nextWeather][2]
-		
-		local success = setNextWeather( )
-		if ( success ) then
-			
-			for key, value in ipairs( getElementsByType("player" ) ) do
-				triggerClientEvent(value, "setPlayerWeather", value, theWeatherID)
-			end	
-		end	
+		setWeather( nextWeather[1] )
+		setNextWeather( )
 	else
-		local success = setNextWeather( )
-		if ( success ) then
-			
-			regulateWeather( )
-		end	
+		setNextWeather( )
+		regulateWeather( )
 	end
 end
 addEventHandler("onResourceStart", resourceRoot, regulateWeather)
@@ -63,59 +39,32 @@ setTimer( regulateWeather, 3600000, 0 )
 
 -- NEXT WEATHER
 function setNextWeather( )
-	nextWeather = math.random(1, 8)
-	
-	if ( nextWeather ) then
-		return true
-	else
-		return false
-	end	
+	nextWeather = weathers[math.random(1, #weathers)]
 end
-
-function getNextWeather( )
-	return nextWeather
-end
-	
--- CURRENT WEATHER	
-function getCurrentWeather( clientCall )
-	if ( not clientCall ) then
-		return theWeatherName, theWeatherID
-	else
-		triggerClientEvent(source, "setPlayerWeather", source, theWeatherID)
-	end	
-end
-addEvent("getCurrentWeather", true)
-addEventHandler("getCurrentWeather", root, getCurrentWeather)
 
 -- ADMIN COMMANDS
-function setWeather( thePlayer, commandName, givenWeatherID )
+function adminSetWeather( thePlayer, commandName, givenWeatherID )
 	if getData(thePlayer, "loggedin") == 1 and exports['[ars]global']:isPlayerTrialModerator(thePlayer) then
 		
 		if (givenWeatherID) then
 			
 			local givenWeatherID = tonumber( givenWeatherID )
-			if ( givenWeatherID >= 1 and givenWeatherID <= 9 ) then
+			local weather = weathers[givenWeatherID]
+			if weather then
+				setWeather(weather[1])
+				setNextWeather()
 				
-				theWeatherID = weathers[givenWeatherID][1]
-				theWeatherName = weathers[givenWeatherID][2]
-				
-				getNextWeather( )
-				
-				for key, value in ipairs( getElementsByType("player" ) ) do
-					triggerClientEvent(value, "setPlayerWeather", value, theWeatherID)
-				end	
-				
-				outputChatBox("The weather was set to '".. tostring(theWeatherName) .."'.", thePlayer, 212, 156, 49)
+				outputChatBox("The weather was set to '".. tostring(weather[2]) .."'.", thePlayer, 212, 156, 49)
 			else
 				for key, value in ipairs ( weathers ) do
 					outputChatBox(tostring( key ) .." - ".. value[2], thePlayer, 212, 156, 49)
 				end	
 				
-				outputChatBox("Weather ID can only be between 1 and 9.", thePlayer, 212, 156, 49)
+				outputChatBox("Weather ID can only be between 1 and " .. #weathers .. ".", thePlayer, 212, 156, 49)
 			end	
 		else
 			outputChatBox("SYNTAX: /".. commandName .." [Weather ID]", thePlayer, 212, 156, 49)
 		end
 	end
 end
-addCommandHandler("setweather", setWeather, false, false)
+addCommandHandler("setweather", adminSetWeather, false, false)
